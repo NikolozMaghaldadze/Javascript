b1537290b570dcc6b3a8d4d8f3f7952561bee5f4
NikaMaghaldadze <nikamaghaldadze13@gmail.com>
2022-10-19 20:34:25 +0400

    davaleba

---
+++ index.js
@@ -0,0 +1,127 @@
+
+//!!!!!! დავალება 1
+
+// let user ={
+//     firstName:"giorgi",
+//     lastName:"smith",
+//     age : 25,
+//     studentStatus: "active"
+// }
+
+// console.log(user.studentStatus);
+
+
+
+//!!!!! დავალება 2
+
+// let massive = ['nika' , 'gio' , 'zura' , 123 , 'kristina' , 'javaScript']
+
+// // for loop
+
+// for (let x = 0; x < massive.length; x++) {
+//     console.log(massive[x]);
+// }
+
+// let z = 0
+
+// //while loop 
+
+// while (z < massive.length) {
+//     console.log(massive[z]);
+//     z++;
+// }
+
+
+// !!!!!! დავალება 3
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for(let x of numbers){
+//     if (x > 5) {
+//         console.log(x);
+//     }
+// }
+
+
+//!!!!!!!! დავალება 4
+
+// let user = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentStatus: 'active'
+// }
+
+// if (user.age < 18 && user.studentStatus == "active") {
+//     console.log("Hello");
+// }else if(user.name == "levani" ) {
+//     console.log("hello levan");
+// }else if(user.studentStatus == "active" || user.age < 25){
+//     console.log("hello world");
+// }else{
+//     console.log("error");
+// }
+
+
+// !!!!!! დავალება 5
+
+// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+
+// for (let y of array) {
+//     if(typeof y === "string"){
+//         console.log(y)
+//     }
+// }
+
+// !!!!!!!! დავალება 6
+
+
+// let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ]
+
+// for(let x of array[0] ){
+//     if (x > 0){
+//         console.log(x);
+//     }
+// }
+
+// for(let x of array[1] ){
+//     if (x > 0){
+//         console.log(x);
+//     }
+// }
+
+// for(let x of array[2] ){
+//     if (x > 0){
+//         console.log(x);
+//     }
+// }
+
+
+// !!!!!!! დავალება 7
+
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+
+// // ლუწებისთვის
+
+// for (let y of array){
+//     if (y % 2 != 1) {
+//         console.log(y);
+//     }
+    
+// }
+
+// // კენტებისთვის
+
+// for (let y of array){
+//     if (y % 2 == 1) {
+//         console.log(y);
+//     }
+    
+// }
+
+
+// !!!!! დავალება 8 ვერ გავაკეთე ;(
+
+
+
+
+
